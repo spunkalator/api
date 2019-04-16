75c5dd390f30befeba56c5d0b2fab473c4ac751a
awele <awele.osuka@gmail.com>
2019-04-16 15:51:33 +0100

    changed config

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     'secret': 'super secret',
     'port' : process.env.PORT||3002,
     dbUrl: {
-        dev: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds229450.mlab.com:29450/heroku_x53h2rvx`,
+        dev: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds229450.mlab.com:${process.env.DB_PORT}/${process.env.DB_NAME}`,
         //dev: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
         prod: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&authSource=admin`,
     }
