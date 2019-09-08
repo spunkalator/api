10e438a19299eff157793cee514bd4abfab94139
awele <awele.osuka@gmail.com>
2019-09-08 22:26:07 +0100

    changed blocked text

--- src/api/app/controller.js
+++ src/api/app/controller.js
@@ -77,7 +77,7 @@ const ObjectId = require('mongodb').ObjectId;
                         console.log(err);
                         return sendErrorResponse(res, {}, 'Something went wrong, please try again');
                     }
-                    return sendSuccessResponse(res, {}, 'History logged');
+                    return sendSuccessResponse(res, {}, 'User has been blocked');
 
             });
 
